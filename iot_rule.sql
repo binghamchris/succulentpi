76e7afb709c4c44e14a3ec68ee7341c8df144ce2
Chris Bingham <chris@pearit.co.uk>
2022-07-12 09:27:11 +0200

    Metrics Ingestion Working
    MQTT test script successfully sending well formatted JSON to AWS IoT Core.
    Working SQL code for the IoT routing rule that ingests messages into AWS Timestream.

---
+++ iot_rule.sql
@@ -0,0 +1,18 @@
+SELECT
+  plant.pot.soil.moisture_top_a0 AS plant_pot_soil_moisture_top,
+  plant.pot.soil.moisture_middle_a1 AS plant_pot_soil_moisture_middle,
+  plant.pot.soil.moisture_bottom_a2 AS plant_pot_soil_moisture_bottom,
+  plant.env.visible_light AS plant_env_visiblelight,
+  plant.env.uv_light AS plant_env_uvlight,
+  plant.env.ir_light AS plant_env_irlight,
+  room.env.dew_point AS room_env_dewpoint,
+  room.env.temp AS room_env_temperature,
+  room.env.rel_humid AS room_env_relativehumidity,
+  room.env.abs_humid AS room_env_absolutehumidty,
+  room.env.co2 AS room_env_co2,
+  room.env.voc_total AS room_env_voctotal,
+  room.env.voc_h2 AS room_env_voch2,
+  room.env.voc_ethanol AS room_env_vocethanol,
+  room.env.pm25 AS room_env_pm25
+FROM
+  'succulentpi/plant1'
--- mqtt_test.py
+++ mqtt_test.py
@@ -28,21 +28,21 @@ mqtt_client_id = config['AWS_IOT_MQTT']['client_id']
 mqtt_certificate= config['AWS_IOT_MQTT']['certificate']
 mqtt_private_key = config['AWS_IOT_MQTT']['private_key']
 mqtt_root_ca = config['AWS_IOT_MQTT']['amazon_root_ca_1']
-topic = config['AWS_IOT_MQTT']['endpoint']
+topic = config['AWS_IOT_MQTT']['topic']
 
 event_loop_group = io.EventLoopGroup(1)
 host_resolver = io.DefaultHostResolver(event_loop_group)
 client_bootstrap = io.ClientBootstrap(event_loop_group, host_resolver)
 mqtt_connection = mqtt_connection_builder.mtls_from_path(
-            endpoint=mqtt_endpoint,
-            cert_filepath=mqtt_certificate,
-            pri_key_filepath=mqtt_private_key,
-            client_bootstrap=client_bootstrap,
-            ca_filepath=mqtt_root_ca,
-            client_id=mqtt_client_id,
-            clean_session=False,
-            keep_alive_secs=6
-            )
+  endpoint=mqtt_endpoint,
+  cert_filepath=mqtt_certificate,
+  pri_key_filepath=mqtt_private_key,
+  client_bootstrap=client_bootstrap,
+  ca_filepath=mqtt_root_ca,
+  client_id=mqtt_client_id,
+  clean_session=False,
+  keep_alive_secs=6
+)
 
 print("Connecting to {} with client ID '{}'...".format(mqtt_endpoint, mqtt_client_id))
 
@@ -51,82 +51,83 @@ connect_future.result()
 print("Connected!")
 
 while True:
-    try:
-      time.sleep(5)
-      print("-------------------")
+  try:
+    time.sleep(10)
+    print("-------------------")
 
-      # Moisture Sensors
-      ## Min  Typ  Max  Condition
-      ## 0    0    0    sensor in open air
-      ## 0    20   300  sensor in dry soil
-      ## 300  580  700  sensor in humid soil
-      ## 700  940  950  sensor in water
+    # Moisture Sensors
+    ## Min  Typ  Max  Condition
+    ## 0    0    0    sensor in open air
+    ## 0    20   300  sensor in dry soil
+    ## 300  580  700  sensor in humid soil
+    ## 700  940  950  sensor in water
 
-      # The seeed_si114x module states that to obtain the correct value, the return from the .ReadUV function must be divided by 100
+    # The seeed_si114x module states that to obtain the correct value, the return from the .ReadUV function must be divided by 100
 
-      data_dict = {
-        "timestamp": datetime.now(),
-        "plant": {
-          "pot": {
-            "soil": {
-              "moisture_top_a0": grovepi.analogRead(0),
-              "moisture_middle_a1": grovepi.analogRead(1),
-              "moisture_bottom_a2": grovepi.analogRead(2)
-            }
-          }
-          "env": {
-            "visible_light": SI1145.ReadVisible,
-            "uv_light": SI1145.ReadUV/100,
-            "ir_light": SI1145.ReadIR
+    data_dict = {
+      "timestamp": datetime.now(),
+      "plant": {
+        "pot": {
+          "soil": {
+            "moisture_top_a0": grovepi.analogRead(0),
+            "moisture_middle_a1": grovepi.analogRead(1),
+            "moisture_bottom_a2": grovepi.analogRead(2)
           }
         },
-        "room" :{
-          "env": {}
+        "env": {
+          "visible_light": SI1145.ReadVisible,
+          "uv_light": SI1145.ReadUV/100,
+          "ir_light": SI1145.ReadIR
         }
+      },
+      "room" :{
+        "env": {}
       }
+    }
 
-      awair_raw = requests.get(config['AWAIR']['local_api_url'])
+    awair_raw = requests.get(config['AWAIR']['local_api_url'])
 
-      # Check if the Awair Local API returned content
-      ## NB: Sometimes it returns HTTP200 with no content
-      if awair_raw.text:
-        awair_json = json.loads(awair_raw.text)
+    # Check if the Awair Local API returned content
+    ## NB: Sometimes it returns HTTP200 with no content
+    if awair_raw.text:
+      awair_json = json.loads(awair_raw.text)
         
-        # Dew point in ºC
-        data_dict['room']['env']['dew_point'] = awair_json['dew_point']
-        # Temperature in ºC
-        data_dict['room']['env']['temp'] = awair_json['temp']
-        # Relative humidity in %
-        data_dict['room']['env']['rel_humid'] = awair_json['humid']
-        # Absolute humidity in g/m³
-        data_dict['room']['env']['abs_humid'] = awair_json['abs_humid']
-        # CO2 in ppm
-        data_dict['room']['env']['co2'] = awair_json['co2']
-        # Total VOCs in ppb
-        data_dict['room']['env']['voc_total'] = awair_json['voc']
-        # Hydrogen sensor signal (unitless)
-        data_dict['room']['env']['voc_h2'] = awair_json['voc_h2_raw']
-        # Ethanol sensor signal (unitless)
-        data_dict['room']['env']['voc_ethanol'] = awair_json['voc_ethanol_raw']
-        # Particulates < 2.5 microns in size in µg/m³
-        data_dict['room']['env']['pm25'] = awair_json['pm25']
-      else:
-        data_dict['room']['env']['dew_point'] = None
-        data_dict['room']['env']['temp'] = None
-        data_dict['room']['env']['rel_humid'] = None
-        data_dict['room']['env']['abs_humid'] = None
-        data_dict['room']['env']['co2'] = None
-        data_dict['room']['env']['voc_total'] = None
-        data_dict['room']['env']['voc_h2'] = None
-        data_dict['room']['env']['voc_ethanol'] = None
-        data_dict['room']['env']['pm25'] = None
+      # Dew point in ºC
+      data_dict['room']['env']['dew_point'] = awair_json['dew_point']
+      # Temperature in ºC
+      data_dict['room']['env']['temp'] = awair_json['temp']
+      # Relative humidity in %
+      data_dict['room']['env']['rel_humid'] = awair_json['humid']
+      # Absolute humidity in g/m³
+      data_dict['room']['env']['abs_humid'] = awair_json['abs_humid']
+      # CO2 in ppm
+      data_dict['room']['env']['co2'] = awair_json['co2']
+      # Total VOCs in ppb
+      data_dict['room']['env']['voc_total'] = awair_json['voc']
+      # Hydrogen sensor signal (unitless)
+      data_dict['room']['env']['voc_h2'] = awair_json['voc_h2_raw']
+      # Ethanol sensor signal (unitless)
+      data_dict['room']['env']['voc_ethanol'] = awair_json['voc_ethanol_raw']
+      # Particulates < 2.5 microns in size in µg/m³
+      data_dict['room']['env']['pm25'] = awair_json['pm25']
+    else:
+      data_dict['room']['env']['dew_point'] = None
+      data_dict['room']['env']['temp'] = None
+      data_dict['room']['env']['rel_humid'] = None
+      data_dict['room']['env']['abs_humid'] = None
+      data_dict['room']['env']['co2'] = None
+      data_dict['room']['env']['voc_total'] = None
+      data_dict['room']['env']['voc_h2'] = None
+      data_dict['room']['env']['voc_ethanol'] = None
+      data_dict['room']['env']['pm25'] = None
 
-      data_json = json.dumps(data_dict, default=str)
-      print(data_json)
 
-      mqtt_connection.publish(topic=config['AWS_IOT_MQTT']['topic'], payload=data_json, qos=mqtt.QoS.AT_LEAST_ONCE)
+    data_json = json.dumps(data_dict, default=str)
+    print(data_json)
 
-    except KeyboardInterrupt:
-        break
-    except IOError:
-        print ("Error")
+    mqtt_connection.publish(topic=topic, payload=data_json, qos=mqtt.QoS.AT_LEAST_ONCE)
+
+  except KeyboardInterrupt:
+    break
+  except IOError:
+    print ("Error")
